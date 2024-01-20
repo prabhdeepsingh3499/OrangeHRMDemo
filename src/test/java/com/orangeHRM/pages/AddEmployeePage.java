40be593aa38636d169351903fbe648f2911c4c2f
Prabhdeep Singh <prabhdeepsingh329@gmail.com>
2024-01-20 12:31:25 +0530

    Added Logging Code
    Added Logging Code

--- src/test/java/com/orangeHRM/pages/AddEmployeePage.java
+++ src/test/java/com/orangeHRM/pages/AddEmployeePage.java
@@ -1,9 +1,13 @@
 package com.orangeHRM.pages;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import com.orangeHRM.utility.LoggerHelper;
 import com.orangeHRM.utility.RandomData;
 public class AddEmployeePage {
 	private WebDriver driver;
+	private Logger logger = LoggerHelper.getLogger(this.getClass());
 	public AddEmployeePage(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -14,9 +18,11 @@ public class AddEmployeePage {
 		RandomData random = new RandomData();
 		driver.findElement(firstName).sendKeys(random.getFirstName());
 		driver.findElement(lastName).sendKeys(random.getLastName());
+		logger.info("Employee name details entered");
 	}
 	public void clickSave() {
 		driver.findElement(save).click();
+		logger.info("Save button clicked successfully");
 	}
 
 }
